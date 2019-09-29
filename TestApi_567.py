9bb2b7814a9304f9abda482a1622643b59dc00db
woshitc8535 <47222350+woshitc8535@users.noreply.github.com>
2019-09-29 17:51:22 -0400

    Update TestApi_567.py

--- TestApi_567.py
+++ TestApi_567.py
@@ -1,16 +1,18 @@
 import unittest
-
-from Api_567 import get_github
+from unittest.mock import patch
 
 
 class BuggyApiTest(unittest.TestCase):
-    def test_check_api(self):
-        self.assertEqual(get_github('richkempinski'),
-                         ['Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6',
-                          'Repo: Mocks Number of commits: 10', 'Repo: Project1 Number of commits: 2',
-                          'Repo: threads-of-life Number of commits: 1']
-                         )
 
+    @patch('Api_567.get_github')
+    def test_check_api(self, mock_get_github):
+        mock_get_github.return_value = ['Repo: hellogitworld Number of commits: 30',
+                                        'Repo: helloworld Number of commits: 6',
+                                        'Repo: Mocks Number of commits: 10', 'Repo: Project1 Number of commits: 2',
+                                        'Repo: threads-of-life Number of commits: 1']
+        self.assertEqual(['Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6',
+                          'Repo: Mocks Number of commits: 10', 'Repo: Project1 Number of commits: 2',
+                          'Repo: threads-of-life Number of commits: 1'],mock_get_github('richkempinski'))
 
 if __name__ == '__main__':
     print('Running unit tests')
